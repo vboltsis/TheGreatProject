5a68a49a35f966d5e38ef090f09fb072f9396312
Nikos Salichos <niksal1991@gmail.com>
2020-12-29 19:22:47 +0200

    TPG-03 Added method Nikos

--- Program.cs
+++ Program.cs
@@ -7,11 +7,19 @@ namespace TheGreatProject
         static void Main(string[] args)
         {
             Print();
+            Nikos();
         }
 
         static void Print()
         {
             Console.WriteLine("Hello World");
         }
+
+
+        static void Nikos()
+        {
+            Console.WriteLine("Hello Nikos - TPG-03");
+        }
+
     }
 }
